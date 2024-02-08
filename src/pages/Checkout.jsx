75b038e410a3df85a9afee7a7cf318dc0b8f5fd7
Ctato1 <dznelashvilitato@gmail.com>
2024-02-08 21:08:47 +0400

    add checkout css

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,16 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/checkout.css";
 import { Container, Row, Col } from "reactstrap";
+import { useState } from "react";
 
 const Checkout = () => {
   const navigate = useNavigate();
 
+  const [payment, setPayment] = useState(false);
   return (
     <Container>
       <Row>
         <Col lg="12">
-          <div className="go-back">
-            <span onClick={() => navigate(-1)}>Go Back</span>
+          <div className="go-back mt-3 mb-5">
+            <span onClick={() => navigate(-1)} style={{ cursor: "pointer" }}>
+              Go Back
+            </span>
           </div>
         </Col>
       </Row>
@@ -20,15 +24,26 @@ const Checkout = () => {
             <form>
               <h3>CHECKOUT</h3>
               <p>BILLING DETAILS</p>
-              <div className="form-first">
-                <label htmlFor="nameInput">Name</label>
-                <input id="nameInput" type="text" placeholder="John Doe" />
-                <label htmlFor="emailInput">Email Address</label>
-                <input
-                  id="emailInput"
-                  type="email"
-                  placeholder="example@gmail.com"
-                />
+              <div className="form-first desktop-field">
+                <div className="desktop-div">
+                  <label htmlFor="nameInput">Name</label>
+                  <input
+                    id="nameInput"
+                    type="text"
+                    placeholder="John Doe"
+                    required
+                  />
+                </div>
+
+                <div className="desktop-div">
+                  <label htmlFor="emailInput">Email Address</label>
+                  <input
+                    id="emailInput"
+                    type="email"
+                    placeholder="example@gmail.com"
+                    required
+                  />
+                </div>
               </div>
               <div className="form-second">
                 <label htmlFor="phoneInput">Phone Number</label>
@@ -36,6 +51,7 @@ const Checkout = () => {
                   id="phoneInput"
                   type="number"
                   placeholder="+995 555-555-555"
+                  required
                 />
               </div>
               <p>SHIPPING INFO</p>
@@ -45,13 +61,28 @@ const Checkout = () => {
                   id="addressInput"
                   type="text"
                   placeholder="123 Williams Avenue"
+                  required
                 />
               </div>
-              <div className="form-fourth">
-                <label htmlFor="zipInput">ZIP Code</label>
-                <input id="zipInput" type="text" placeholder="1234" />
-                <label htmlFor="cityInput">City</label>
-                <input id="cityInput" type="text" placeholder="London" />
+              <div className="form-fourth  desktop-field">
+                <div className="desktop-div">
+                  <label htmlFor="zipInput">ZIP Code</label>
+                  <input
+                    id="zipInput"
+                    type="text"
+                    placeholder="1234"
+                    required
+                  />
+                </div>
+                <div className="desktop-div">
+                  <label htmlFor="cityInput">City</label>
+                  <input
+                    id="cityInput"
+                    type="text"
+                    placeholder="London"
+                    required
+                  />
+                </div>
               </div>
               <div className="form-fifth">
                 <label htmlFor="countryInput">Country</label>
@@ -59,22 +90,73 @@ const Checkout = () => {
                   id="countryInput"
                   type="text"
                   placeholder="United Kingdom"
+                  required
                 />
               </div>
               <p>PAYMENT DETAILS</p>
-              <div className="form-sixth">
+              <div className="form-sixth  desktop-field">
                 <h4>Payment Method</h4>
                 <div className="payment-details">
-                  <form className="payment-select">
-                    <input id="moneyBtn" type="radio" name="card"/>
-                    <label for="moneyBtn">e-Money</label>
-                  </form>
-                  <form className="payment-select">
-                    <input id="cashBtn" type="radio" name="card"/>
-                    <label for="cashBtn">Cash on Delivery</label>
-                  </form>
+                  <div
+                    className="payment-select"
+                    style={{ border: payment ? "1px solid red" : "" }}
+                  >
+                    <input
+                      id="moneyBtn"
+                      type="radio"
+                      name="card"
+                      checked={payment ? true : false}
+                      onClick={() => setPayment((prev) => (prev = true))}
+                    />
+                    <label
+                      for="moneyBtn"
+                      onClick={() => setPayment((prev) => (prev = true))}
+                    >
+                      e-Money
+                    </label>
+                  </div>
+                  <div
+                    className="payment-select"
+                    style={{ border: !payment ? "1px solid red" : "" }}
+                  >
+                    <input
+                      id="cashBtn"
+                      type="radio"
+                      name="card"
+                      checked={!payment ? true : false}
+                      onClick={() => setPayment((prev) => (prev = false))}
+                    />
+                    <label
+                      for="cashBtn"
+                      onClick={() => setPayment((prev) => (prev = false))}
+                    >
+                      Cash on Delivery
+                    </label>
+                  </div>
                 </div>
               </div>
+              {payment && (
+                <div className="payment-card desktop-field">
+                  <div className="desktop-div">
+                    <label htmlFor="cardInput">e-Money Number</label>
+                    <input
+                      id="cardInput"
+                      type="number"
+                      placeholder="1111 2222 3333 4444"
+                      required
+                    />
+                  </div>
+                  <div className="desktop-div">
+                    <label htmlFor="pinInput">e-Money PIN</label>
+                    <input
+                      id="pinInput"
+                      type="number"
+                      placeholder="1234"
+                      required
+                    />
+                  </div>
+                </div>
+              )}
             </form>
           </section>
         </Col>
